76164255b12e6e622d4411ef8ba19efaf3ec638c
USER <user@USERs-MacBook-Pro.local>
2022-04-20 16:45:05 -0500

    Added first few functions

--- mfs.c
+++ mfs.c
@@ -59,18 +59,35 @@ undel
 */
 
 void open();
-void close();
+//void close();
 void info();
 void stat();
 void changeDirectory();
 void listFiles();
 void get();
-void read();
+//void read();
 void delete();
 void unDelete();
 
-//
-struct __attribute__((__packed__)) DirectoryEntry {
+
+char BS_OEMName[8];
+int16_t BPB_BytsPerSec;
+int8_t BPB_SecPerClus;
+int16_t BPB_RsvdSecCnt;
+int8_t BPB_NumFATs;
+int16_t BPB_RootEntCnt;
+char BS_VolLab[11];
+int32_t BPB_FATSz32;
+int32_t BPB_RootClus;
+
+int32_t RootDirSectors = 0;
+int32_t FirstDataSector = 0;
+int32_t FirstSectorofCluster = 0;
+
+int opened = 0;
+
+struct __attribute__((__packed__)) DirectoryEntry 
+{
   char DIR_Name[11];
   uint8_t DIR_Attr;
   uint8_t Unused1[8]; 
@@ -86,6 +103,8 @@ int main()
 {
 
   char * cmd_str = (char*) malloc( MAX_COMMAND_SIZE );
+  FILE * fp;
+
 
   while( 1 )
   {
@@ -129,15 +148,67 @@ int main()
 
     // Now print the tokenized input as a debug check
     // \TODO Remove this code and replace with your FAT32 functionality
-
-    int token_index  = 0;
-    for( token_index = 0; token_index < token_count; token_index ++ ) 
+    if(strcmp(token[0],"open") == 0)
     {
-      printf("token[%d] = %s\n", token_index, token[token_index] );  
+      fp = fopen("fat32.img", "r");
+      if(fp == NULL)
+      {
+        printf("Error: File system image not found\n");
+      }
+      else if(opened == 1)
+      {
+        printf("Error: File system image already open\n");
+      }
+      else{
+        opened = 1;
+        fseek(fp, 3, SEEK_SET);
+        fread(&BS_OEMName, 8, 1, fp);
+        fseek(fp, 11, SEEK_SET);
+        fread(&BPB_BytsPerSec, 2, 1, fp);
+        fseek(fp, 13, SEEK_SET);
+        fread(&BPB_SecPerClus, 1, 1, fp);
+        fseek(fp, 14, SEEK_SET);
+        fread(&BPB_RsvdSecCnt, 2, 1, fp);
+        fseek(fp, 16, SEEK_SET);
+        fread(&BPB_NumFATs, 1, 1, fp);
+        fseek(fp, 17, SEEK_SET);
+        fread(&BPB_RootEntCnt, 2, 1, fp);
+        fseek(fp, 36, SEEK_SET);
+        fread(&BPB_FATSz32, 4, 1, fp);
+        fseek(fp, 44, SEEK_SET);
+        fread(&BPB_RootClus, 4, 1, fp);
+        fseek(fp, 71, SEEK_SET);
+        fread(&BS_VolLab, 11, 1, fp);
+      }
+    }
+    if(strcmp(token[0], "close") == 0)
+    {
+      fclose(fp);
+      fp = NULL;
+      opened = 0;
     }
 
-    free( working_root );
+    if(strcmp(token[0],"info") ==0 )
+    {
+      printf("\t\t\tHex:\tDecimal:\n");
+      printf("BPB_BytesPerSec:\t%x\t%d\n", BPB_BytsPerSec, BPB_BytsPerSec);
+      printf("BPB_SecPerClus:\t\t%x\t%d\n", BPB_SecPerClus, BPB_SecPerClus);
+      printf("BPB_RsvdSecCnt:\t\t%x\t%d\n", BPB_RsvdSecCnt, BPB_RsvdSecCnt);
+      printf("BPB_NumFATS:\t\t%x\t%d\n", BPB_NumFATs, BPB_NumFATs);
+      printf("BPB_FATSz32:\t\t%x\t%d\n", BPB_FATSz32, BPB_FATSz32);
+    }
+    
+    if(strcmp(token[0],"stat") ==0)
+    {
 
+    }
+
+    
+    free( working_root );
+  }
+  if(fp != NULL)
+  {
+    fclose(fp);
   }
   return 0;
 }
