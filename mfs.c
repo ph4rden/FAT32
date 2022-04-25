b3af3626b3c4422b50297ae5486712d598f7e064
USER <user@USERs-MacBook-Pro.local>
2022-04-25 18:35:39 -0500

    Whatevers left

--- mfs.c
+++ mfs.c
@@ -34,6 +34,7 @@ Brian Phan - 1001795028 - Section:003
 #include <string.h>
 #include <signal.h>
 #include <stdint.h>
+#include <ctype.h>
 
 #define MAX_NUM_ARGUMENTS 3
 
@@ -44,39 +45,16 @@ Brian Phan - 1001795028 - Section:003
 
 #define MAX_COMMAND_SIZE 255    // The maximum command-line size
 
-/*
-commands:
-open 
-close 
-info 
-stat 
-cd 
-ls 
-get 
-read 
-del 
-undel 
-*/
-
-void open();
-//void close();
-void info();
-void stat();
-void changeDirectory();
-void listFiles();
-void get();
-//void read();
-void delete();
-void unDelete();
 
 
-char BS_OEMName[8];
+
+char    BS_OEMName[8];
 int16_t BPB_BytsPerSec;
-int8_t BPB_SecPerClus;
+int8_t  BPB_SecPerClus;
 int16_t BPB_RsvdSecCnt;
-int8_t BPB_NumFATs;
+int8_t  BPB_NumFATs;
 int16_t BPB_RootEntCnt;
-char BS_VolLab[11];
+char    BS_VolLab[11];
 int32_t BPB_FATSz32;
 int32_t BPB_RootClus;
 
@@ -84,32 +62,81 @@ int32_t RootDirSectors = 0;
 int32_t FirstDataSector = 0;
 int32_t FirstSectorofCluster = 0;
 
-int opened = 0;
+FILE * fp;
+FILE * ofp;
 
 struct __attribute__((__packed__)) DirectoryEntry 
 {
-  char DIR_Name[11];
-  uint8_t DIR_Attr;
-  uint8_t Unused1[8]; 
-  uint16_t DIR_FirstClusterHigh;
-  uint8_t Unused2[4];
-  uint16_t DIR_FirstClusterLow;
-  uint32_t DIR_FileSize;
+  char      DIR_Name[11];
+  uint8_t   DIR_Attr;
+  uint8_t   Unused1[8]; 
+  uint16_t  DIR_FirstClusterHigh;
+  uint8_t   Unused2[4];
+  uint16_t  DIR_FirstClusterLow;
+  uint32_t  DIR_FileSize;
 };
 
 struct DirectoryEntry dir[16];
+
+int LBAToOffset( int32_t sector)
+{
+  return ((sector - 2 ) * BPB_BytsPerSec) + (BPB_BytsPerSec * BPB_RsvdSecCnt) + (BPB_NumFATs * BPB_FATSz32 * BPB_BytsPerSec);
+}
+
+int16_t NextLB( uint32_t sector )
+{
+  uint32_t FATAddress = ( BPB_BytsPerSec * BPB_RsvdSecCnt) + ( sector * 4 );
+  int16_t val; 
+  fseek( fp, FATAddress, SEEK_SET); 
+  fread( &val, 2, 1, fp);
+  return val;
+}
+
+int compare(char* input, char* IMG_Name){
+  char expanded_name[12];
+  memset( expanded_name, ' ', 12 );
+
+  char *token = strtok( input, "." );
+
+  strncpy( expanded_name, token, strlen( token ) );
+
+  token = strtok( NULL, "." );
+
+  if( token )
+  {
+    strncpy( (char*)(expanded_name+8), token, strlen(token ) );
+  }
+
+  expanded_name[11] = '\0';
+
+  int i;
+  for( i = 0; i < 11; i++ )
+  {
+    expanded_name[i] = toupper( expanded_name[i] );
+  }
+
+  if( strncmp( expanded_name, IMG_Name, 11 ) == 0 )
+  {
+    return 1;
+  }
+
+  return 0;
+}
+
+
+int opened = 0;
+char saved_filename[11];
 
 int main()
 {
 
   char * cmd_str = (char*) malloc( MAX_COMMAND_SIZE );
-  FILE * fp;
 
 
   while( 1 )
   {
     // Print out the mfs prompt
-    printf ("mfs> ");
+    printf ("\nmfs> ");
 
     // Read the command from the commandline.  The
     // maximum command that will be read is MAX_COMMAND_SIZE
@@ -150,7 +177,7 @@ int main()
     // \TODO Remove this code and replace with your FAT32 functionality
     if(strcmp(token[0],"open") == 0)
     {
-      fp = fopen("fat32.img", "r");
+      fp = fopen(token[1], "r");
       if(fp == NULL)
       {
         printf("Error: File system image not found\n");
@@ -179,6 +206,11 @@ int main()
         fread(&BPB_RootClus, 4, 1, fp);
         fseek(fp, 71, SEEK_SET);
         fread(&BS_VolLab, 11, 1, fp);
+        fseek(fp, (BPB_NumFATs * BPB_FATSz32 * BPB_BytsPerSec) + (BPB_RsvdSecCnt * BPB_BytsPerSec), SEEK_SET);
+        fread(&dir[0], sizeof(struct DirectoryEntry), 16, fp);
+        for(int i = 0; i < 16; i++){
+          printf("\n%s", dir[i].DIR_Name);
+        }
       }
     }
     if(strcmp(token[0], "close") == 0)
@@ -198,12 +230,119 @@ int main()
       printf("BPB_FATSz32:\t\t%x\t%d\n", BPB_FATSz32, BPB_FATSz32);
     }
     
-    if(strcmp(token[0],"stat") ==0)
+    if(strcmp(token[0],"stat") == 0)
     {
 
     }
 
+    if(strcmp(token[0], "get") == 0){
+      int found = 0;
+      for(int i = 0; i < 16; i++){
+        if(compare(token[1], dir[i].DIR_Name)){
+          found = 1;
+          int cluster = dir[i].DIR_FirstClusterLow;
+          int offset = LBAToOffset(cluster);
+          int size = dir[i].DIR_FileSize;
+          fseek(fp, offset, SEEK_SET);
+          ofp = fopen(token[1], "w");
+          uint8_t buffer[12];
+          while(size > BPB_BytsPerSec){
+            fread(&buffer, 512, 1, fp);
+            fwrite(&buffer, 512, 1, ofp);
+            size = size - 512;
+            cluster = NextLB(cluster);
+            offset = LBAToOffset(cluster);
+            fseek(fp, offset, SEEK_SET);
+          }
+          if(size > 0){
+            fread(&buffer, size, 1, fp);
+            fwrite(&buffer, size, 1, ofp);
+            i = 16;
+            fclose(ofp);
+          }
+        }
+      }
+      if(!found){
+        printf("Error: File not found");
+      }
+
+    }
+
+    if(strcmp(token[0],"ls") == 0)
+    {
+      for(int i=0;i<16;i++)
+      {
+        if((dir[i].DIR_Attr == 0x01) || (dir[i].DIR_Attr == 0x10) || (dir[i].DIR_Attr == 0x20) && (dir[i].DIR_Name[0]!= 0xe5))
+        {
+          char name[12];
+          memcpy(name, dir[i].DIR_Name,11);
+          name[11] = '\0';
+          printf("%s\n",name);
+        }
+      }
+    }
+
+    if(strcmp(token[0],"cd") == 0)
+    {
+      if(token[1] == "..")
+      {
+
+      }
+      for(int i=0; i<16; i++)
+      {
+        if(compare(token[1], dir[i].DIR_Name))
+        {
+          int cluster =  dir[i].DIR_FirstClusterLow;
+          int offset = LBAToOffset(cluster);
+          fseek(fp, offset, SEEK_SET);
+          fread(&dir[0],sizeof(struct DirectoryEntry),16,fp);
+          break;
+        }
+      }
+      
+    }
     
+    if(strcmp(token[0],"read") == 0)
+    {
+      
+      
+    }
+
+    if(strcmp(token[0],"del") == 0)
+    {
+      if(token[1] == NULL){
+        printf("\nNot enough arguments for del");
+      }
+      else{
+        int found = 0;
+        for(int i = 0; i < 16; i++){
+          if(compare(token[1], dir[i].DIR_Name)){
+            strncpy(saved_filename, dir[i].DIR_Name, 11);
+            dir[i].DIR_Name[0] = '?';
+            dir[i].DIR_Attr = 0;
+            found = 1;
+          }
+        }
+        if(!found){
+          printf("\nDirectory or File did not exist");
+        }
+      }
+    }
+
+    if(strcmp(token[0],"undel") == 0)
+    {
+      if(token[1] == NULL){
+        printf("\nNot enough arguments for undel");
+      }
+      else{
+        for(int i = 0; i < 16; i++){
+          if(dir[i].DIR_Name[0] == '?'){
+            strncpy(dir[i].DIR_Name, saved_filename, 11);
+            dir[i].DIR_Attr = 0x1;
+          }
+        }
+      }
+    }
     free( working_root );
   }
   if(fp != NULL)
